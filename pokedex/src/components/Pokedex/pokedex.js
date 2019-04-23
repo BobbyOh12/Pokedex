f38b909128b1aa65ac359f795b020bcc72cd2382
Bobby.Oh <bawbee.oh@gmail.com>
2019-04-23 14:34:08 -0700

    save

---
+++ pokedex/src/components/Pokedex/pokedex.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './pokedex.css'
+
+export default class Pokedex extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="pokedex">
+                Test
+            </div>
+        )
+    }
+}
